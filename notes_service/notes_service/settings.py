3ba8a259e291db7347d4a084f7a5f33ee0d129e0
josignaciocab <joseignaciocabrerabustamante@gmail.com>
2024-04-15 20:57:41 -0400

    feat1: modified db

--- notes_service/notes_service/settings.py
+++ notes_service/notes_service/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'notes_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'postgres',
-        # 'USER': 'db_user',
-        # 'PASSWORD': 'super_user',
-        # 'HOST': 'notes1.c16ywqwiknc3.us-east-2.rds.amazonaws.com',
-        # 'PORT': '5432'
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'db_user',
+        'PASSWORD': 'super_user',
+        'HOST': 'notes1.c16ywqwiknc3.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
